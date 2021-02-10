b7a5da8f71637c0dc966589cfe0bdd40af9ff7b3
alikhamesy <alikhamesy0@gmail.com>
2021-02-09 20:15:07 -0700

    workshop code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from 'react'
 
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import styles from './App.module.css'
+
+import myImgSrc from './logo.svg'
+
+const VoteableMessage = props => {
+  const [likes, setLikes] = useState(0)
+  const [dislikes, setDislikes] = useState(0)
+  const myRef = useRef(null)
+
+  console.log('componentWillMount AND componentWillUpdate')
+  console.log(myRef)
+
+  useEffect(() => {
+    console.log('componentDidMount')
+    console.log(myRef)
+
+    return () => {
+      console.log('componentDidUnmout')
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log('componentDidUpdate')
+    console.log(myRef)
+  })
+
+  useEffect(() => {
+    console.log('new like.')
+  }, [likes])
+
+  const upvote = () => {
+    setLikes(likes + 1)
+  }
+  const downvote = () => {
+    setDislikes(dislikes + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={myRef}>
+      {props.username && (
+        <div>
+          <h6 id={styles.myClassName}>{props.username}</h6>
+          <p>{props.message}</p>
+          <button onClick={downvote}>Dislike</button>
+          <span>{likes - dislikes}</span>
+          <button onClick={upvote}>Like</button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+const CreateMessage = props => {
+  const [currMsg, setCurrMsg] = useState('')
+  console.log(currMsg)
+  const handleChange = event => {
+    setCurrMsg(event.target.value)
+  }
+
+  const createNewMessage = () => {
+    props.onCreate(currMsg)
+  }
+
+  return (
+    <div>
+      <input placeholder='message...' onChange={handleChange}></input>
+      <button onClick={createNewMessage}>Create Message</button>
+    </div>
+  )
+}
+
+function App() {
+  const [messages, setMessages] = useState([])
+
+  const handleCreate = newMessage => {
+    messages.push(newMessage)
+    setMessages([...messages])
+  }
+
+  const [k, setK] = useState(0)
+
+  // cycle dep nuke: ENABLE AT OWN RISK
+  // useEffect(() => setK(k + 1), [k])
+
+  return (
+    <div>
+      <img alt='react logo' />
+      <p>{k}</p>
+      <CreateMessage onCreate={handleCreate} />
+      {/* <VoteableMessage username='pilot' message='hello' /> */}
+
+      {messages.map(msg => (
+        <VoteableMessage message={msg} username='pilot' />
+      ))}
+    </div>
+  )
+}
+export default App
